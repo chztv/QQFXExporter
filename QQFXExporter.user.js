81b3a48a32803d2c60c3bca3789ae2ecd0973890
chztv <0571chz@gmail.com>
2012-12-05 09:49:45 +0800

    Ver 0.1
    第一个版本

---
+++ QQFXExporter.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name       QQLixianExporter
+// @namespace  http://fenxiang.qq.com/
+// @version    0.01
+// @description  export QQ lixian url to aria2/wget
+// @match      http://fenxiang.qq.com/x/*
+// @run-at document-end
+// @copyright  2012+, Binux <root@binux.me>
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.id = "TLE_script";
+if (location.host == "fenxiang.qq.com") {
+  script.src = "http://yx.hoolo.tv/QQLixian/QQLixianExporter.js";
+} else {
+  script.src = "http://yx.hoolo.tv/QQLixian/vod_html5.js";
+}
+document.body.appendChild(script);
