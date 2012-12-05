12b97e88be8c30b63dffbd0789e68004d1853f40
chztv <0571chz@gmail.com>
2012-12-05 13:22:38 +0800

    29

--- QQFXExporter.js
+++ QQFXExporter.js
@@ -58,6 +58,7 @@ function start_normal_down_paul(filename,filehash){
 //保存按钮
 $('.setting_button').live("click",function(){		    
 	//获取选择的列表
+	TLE.setConfig("TLE_aria2_jsonrpc", $("#QQ_aria2_jsonrpc").val());
 	XF.widget.msgbox.show(TLE.getConfig("TLE_aria2_jsonrpc"),2,2000);
 } );
 
